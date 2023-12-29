8a8f8ae81f368489d2c08bdd4d087967727977b1
Kiru276 <diego.777.meza@gmail.com>
2023-12-29 03:35:18 -0300

    mod aplicacion de documentacion antes de creacion reserva teorica

--- frontend/src/routes/Reserve.jsx
+++ frontend/src/routes/Reserve.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from '../context/AuthContext';
 import { createReserve1 } from '../services/reserve.service.js';
+import { getAllPostulation } from '../services/postulation.service.js';
 import Swal from 'sweetalert2';
 import '../css/reserveStyles.css';
 
@@ -22,6 +23,33 @@ function PageReserve() {
     return <p>Usuario no autenticado</p>;
   }
 
+  const [postulation, setPostulation] = useState([]);
+
+  useEffect(() => {
+    const fetchPostulation = async () => {
+      try {
+        const response = await getAllPostulation();
+  
+        // Encuentra la postulación correspondiente al usuario actual
+        const userPostulation = response.data.find(
+          (item) => item.solicitanteId._id === user.id
+        );
+  
+        if (userPostulation) {
+          console.log('Postulación encontrada');
+          setPostulation(userPostulation);
+        } else {
+          console.log('Postulación no encontrada para el usuario con ID');
+        }
+      } catch (error) {
+        console.error('Error al obtener las postulaciones:', error);
+      }
+    };
+  
+    fetchPostulation();
+  }, [user]);
+  
+
   const [formData, setFormData] = useState({
     fechaReserva: '',
     horaReserva: '',
@@ -36,59 +64,72 @@ function PageReserve() {
   };
 
   const handleSubmit = async (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  // Validar que los campos estén completos
-  if (!formData.fechaReserva || !formData.horaReserva) {
-    Swal.fire({
-      title: 'Atención',
-      text: 'Por favor, complete todos los campos del formulario',
-      icon: 'warning',
-    });
-    return;
-  }
-
-  try {
-    // Utiliza la función createReserve1 del servicio
-    const response = await createReserve1({
-      ...formData,
-      solicitanteId: user ? user.id : '',
-    });
-
-    if (response.status === 201) {
-      Swal.fire({
-        title: 'Todo correcto',
-        text: 'La reserva se creó correctamente',
-        icon: 'success',
-      });
-    } else if (response.status === 400) {
+    // Validar que los campos estén completos
+    if (!formData.fechaReserva || !formData.horaReserva) {
       Swal.fire({
         title: 'Atención',
-        text: 'Datos ingresados incorrectos, ingrese los datos de forma correcta',
+        text: 'Por favor, complete todos los campos del formulario',
         icon: 'warning',
       });
-    } else if (response.status === 500) {
+      return;
+    }
+
+    try {
+      console.log('Postulación actual en el estado');
+  
+      // Verificar si existe una postulación y su estado es "Aprobado"
+      if (postulation && postulation.estadoReserva === 'Aceptado') {
+        console.log('La postulación está aprobada, procediendo a crear la reserva...');
+  
+        // Utiliza la función createReserve1 del servicio
+        const response = await createReserve1({
+          ...formData,
+          solicitanteId: user ? user.id : '',
+        });
+
+        if (response.status === 201) {
+          Swal.fire({
+            title: 'Todo correcto',
+            text: 'La reserva se creó correctamente',
+            icon: 'success',
+          });
+        } else if (response.status === 400) {
+          Swal.fire({
+            title: 'Atención',
+            text: 'Datos ingresados incorrectos, ingrese los datos de forma correcta',
+            icon: 'warning',
+          });
+        } else if (response.status === 500) {
+          Swal.fire({
+            title: 'ERROR',
+            text:
+              'Hubo un error interno en el servidor. Por favor, inténtalo de nuevo más tarde.',
+            icon: 'error',
+          });
+        } else {
+          Swal.fire({
+            title: 'ERROR',
+            text: 'Hubo un error al enviar la reserva. Por favor, inténtalo de nuevo.',
+            icon: 'error',
+          });
+        }
+      } else {
+        Swal.fire({
+          title: 'Atención',
+          text: 'No puedes realizar reservas si tu postulación no está aprobada.',
+          icon: 'warning',
+        });
+      }
+    } catch (error) {
       Swal.fire({
         title: 'ERROR',
-        text:
-          'Hubo un error interno en el servidor. Por favor, inténtalo de nuevo más tarde.',
-        icon: 'error',
-      });
-    } else {
-      Swal.fire({
-        title: 'ERROR',
-        text: 'Hubo un error al enviar la reserva. Por favor, inténtalo de nuevo.',
+        text: 'Hubo un error inesperado, inténtelo más tarde.',
         icon: 'error',
       });
     }
-  } catch (error) {
-    Swal.fire({
-      title: 'ERROR',
-      text: 'Hubo un error inesperado, inténtelo más tarde.',
-      icon: 'error',
-    });
-  }
-};
+  };
 
   return (
     <div className="reserve-container">
@@ -130,4 +171,4 @@ function PageReserve() {
   );
 }
 
-export default PageReserve;
+export default Reserve;
